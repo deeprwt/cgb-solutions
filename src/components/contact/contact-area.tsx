04d1439f0d1fc1fb5acd1202ef6dd388e5e084bc
Deepak Rawat <rwtroshan28@gmail.com>
2024-03-13 13:10:41 +0530

    update contact us page

--- src/components/contact/contact-area.tsx
+++ src/components/contact/contact-area.tsx
@@ -36,35 +36,8 @@ const ContactArea = () => {
         <div className="position-relative">
           <div className="row">
             {/* Location data import */}
-            <LocationTabsData/>
+            <LocationTabsData />
             {/* Location data import */}
-            {/* <div className="col-12 m-auto pt-30">
-              <div className="row">
-                <div className="col-md-6">
-                  <AddressBlock
-                    icon={icon_1}
-                    title="Our Address"
-                    subtitle="46/1, NH 44, Near Krishan Reddy Industrial Area, Kudlu Gate, Hosapalaya, Garvebhavi Palya Bengaluru, Karnataka, 560068"
-                  />
-                </div>
-                <div className="col-md-6">
-                  <AddressBlock
-                    icon={icon_2}
-                    title="Contact Info"
-                    subtitle='Drop a mail at :
-                    <a href="mailto:info@cgbindia.com" className="call text-lg fw-500">info@cgbindia</a> <br />
-                    Give us call at : <a href="tel:8287668958" className="call text-lg fw-500">8287668958</a>'
-                  />
-                </div>
-                {/* <div className="col-md-4">
-                  <AddressBlock
-                    icon={icon_3}
-                    title="Live Support"
-                    subtitle='live chat service <br/><a href="#" className="webaddress">www.cgbindia.com</a>'
-                  />
-                </div> 
-              </div>
-            </div> */}
           </div>
 
           <div className="bg-wrapper light-bg mt-80 lg-mt-40">
@@ -103,8 +76,10 @@ const ContactArea = () => {
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d80824.64108959114!2d77.57106061643262!3d12.940131448526687!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae16986c307911%3A0x9be62fff97b110aa!2sCGB%20Solutions%20PVT.%20LTD.!5e0!3m2!1sen!2sin!4v1708347527779!5m2!1sen!2sin"
           ></iframe> */}
           <iframe
-           className="gmap_iframe h-100 w-100"
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d979.4739631226944!2d77.6422128494832!3d12.892181344459578!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae14bad2ab0221%3A0x8bd04d73b003661f!2sAKR%20Tech%20Park!5e0!3m2!1sen!2sin!4v1710137545208!5m2!1sen!2sin"  loading="lazy"></iframe>
+            className="gmap_iframe h-100 w-100"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d979.4739631226944!2d77.6422128494832!3d12.892181344459578!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae14bad2ab0221%3A0x8bd04d73b003661f!2sAKR%20Tech%20Park!5e0!3m2!1sen!2sin!4v1710137545208!5m2!1sen!2sin"
+            loading="lazy"
+          ></iframe>
           {/* <iframe
             className="gmap_iframe h-100 w-100"
             src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=dhaka collage&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
