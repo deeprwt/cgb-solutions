8ae494fcaea0e3eb4ec375fcf06013a51ec8238d
Deepak Rawat <rwtroshan28@gmail.com>
2024-04-07 14:18:28 +0530

    adding g-tag google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,14 @@
 import type { Metadata } from "next";
-// adding this gtag 
-import Script from 'next/script'
+import { GoogleAnalytics } from "@next/third-parties/google";
 // adding this gtag
 
 import "./globals.scss";
 import { Providers } from "@/redux/provider";
 
-
 export const metadata: Metadata = {
   title: "CGB Solutions",
-  description: "Empower your business with CGB Solutions' IT expertise. Strategic consulting & managed services for peak performance. Contact us.",
+  description:
+    "Empower your business with CGB Solutions' IT expertise. Strategic consulting & managed services for peak performance. Contact us.",
 };
 
 export default function RootLayout({
@@ -19,28 +18,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      {/* staring here gtag */}
-      <head>
-
-<Script
-  async
-  src="https://www.googletagmanager.com/gtag/js?id=G-NYNVJH0DG6"
-/>
-
-<Script id="google-analytics">
-  {`
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});
-  `}
-</Script>
-</head>
-      {/* End here gtag */}
-
       <body>
         <Providers>{children}</Providers>
       </body>
+      {/* staring here gtag */}
+      <GoogleAnalytics gaId="G-NYNVJH0DG6" />
+      {/* End here gtag */}
     </html>
   );
 }
