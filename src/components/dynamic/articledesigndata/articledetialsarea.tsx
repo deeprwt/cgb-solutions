ba2071934aae40cce122c367fe6002a4c9a3ceb3
Deepak Rawat <rwtroshan28@gmail.com>
2024-12-21 13:57:48 +0530

    updated article card design

--- src/components/dynamic/articledesigndata/articledetialsarea.tsx
+++ src/components/dynamic/articledesigndata/articledetialsarea.tsx
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from "react";
 import UseSocialShare from "@/components/socail-media/socialshare";
 import BlogSidebar from "@/components/blogs/blog-sidebar";
+import Image, { StaticImageData } from "next/image";
 
 type Article = {
   id?: string;
@@ -23,17 +24,25 @@ const ArticleDetailsArea: React.FC<{ article: Article }> = ({ article }) => {
   }
 
   return (
-    <div className="blog-details position-relative mt-150 lg-mt-80 mb-150 lg-mb-80">
+    <div className="blog-details position-relative mt-80 lg-mt-80 mb-150 lg-mb-80">
       <div className="container">
         <div className="row gx-xl-5">
-          <div className="col-lg-8">
+          <div className="col-lg-12">
             <article className="blog-meta-two style-two">
-              <figure
+              {/* <figure
                 className="post-img position-relative d-flex align-items-end m0"
                 style={{ backgroundImage: `url(${article.imageUrl || article.image})` }}
               >
                 <div className="date">{article.date}</div>
-              </figure>
+              </figure> */}
+               <Image
+          src={article.imageUrl || article.image}
+          alt={article.title || "Article Image"}
+          layout="responsive"
+          width={600}
+          height={400}
+          className="lazy-img me-auto ms-auto"
+        />
               <div className="post-data">
                 <div className="post-info">{article.category}</div>
                 <div className="blog-title">
@@ -48,9 +57,9 @@ const ArticleDetailsArea: React.FC<{ article: Article }> = ({ article }) => {
               </div>
             </article>
           </div>
-          <div className="col-lg-4 col-md-8">
+          {/* <div className="col-lg-4 col-md-8">
             <BlogSidebar />
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
--- src/components/dynamic/articledesigndata/articlewebsitecard.tsx
+++ src/components/dynamic/articledesigndata/articlewebsitecard.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Link from "next/link";
+import Image, { StaticImageData } from "next/image";
 
 type ArticleCardProps = {
   article: {
@@ -18,21 +19,39 @@ type ArticleCardProps = {
 const ArticleWebsiteCard: React.FC<ArticleCardProps> = ({ article }) => {
   return (
     <article className="blog-meta-two style-two mb-50 lg-mb-40">
-      <figure
+      {/* <figure
         className="post-img position-relative d-flex align-items-end m0"
         style={{ backgroundImage: `url(${article.imageUrl || article.image})`, height: "252px" }}
       >
         <Link href={`/article/${article.link}`} className="stretched-link date tran3s">
           {article.date.split(" ")[0]} {article.date.split(" ")[1]}
         </Link>
-      </figure>
+      </figure> */}
+      <Link href={`/article/${article.link}`} className="date tran3s w-100">
+        <Image
+          src={article.imageUrl || article.image}
+          alt={article.title || "Article Image"}
+          layout="responsive"
+          width={600}
+          height={400}
+          className="lazy-img me-auto ms-auto"
+        />
+      </Link>
       <div className="post-data">
-        <div className="post-info">{article.category}</div>
+        <div className="post-info d-flex justify-content-between align-items-center">
+          <span>{article.category} </span>{" "}
+          <span className="articledate">
+            {article.date.split(" ")[0]} {article.date.split(" ")[1]}
+          </span>
+        </div>
         <div className="d-flex justify-content-between align-items-center flex-wrap">
           <Link href={`/article/${article.link}`} className="blog-title">
             <h4 style={{ fontSize: "23px" }}>{article.title}</h4>
           </Link>
-          <Link href={`/article/${article.link}`} className="round-btn rounded-circle d-flex align-items-center justify-content-center tran3s">
+          <Link
+            href={`/article/${article.link}`}
+            className="round-btn rounded-circle d-flex align-items-center justify-content-center tran3s"
+          >
             <i className="bi bi-arrow-up-right"></i>
           </Link>
         </div>
