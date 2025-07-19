e9976ff3de8e309247d12a50d49dd61f094e6f8b
Deepak Rawat <rwtroshan28@gmail.com>
2025-07-19 13:49:02 +0530

    update it manged

--- src/data/menu-data.ts
+++ src/data/menu-data.ts
@@ -101,7 +101,7 @@ const menu_data: IMenu[] = [
     title: "Services",
     sub_dropdown: true,
     dropdown_submenus: [
-      { link: "/solutions/infrastructure-management", title: "Infrastructure Management" },
+      { link: "/solutions/it-managed", title: "It Managed Services" },
       { link: "/solutions/oracle", title: "Oracle" },
       { link: "/solutions/finacle", title: "Finacle Core Banking" },
       { link: "/solutions/servicenow", title: "Service Now" },
