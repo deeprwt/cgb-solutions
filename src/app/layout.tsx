8e30ad3a9be28231217d89afbdb1983893bf911c
deeprwt <rwtroshan28@gmail.com>
2026-02-16 16:31:04 +0530

    Apollo Website Tracker

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,6 +38,27 @@ export default function RootLayout({
           `}
         </Script>
         {/* End Google Tag Manager */}
+        {/* Apollo Website Tracker */}
+        <Script id="apollo-tracker" strategy="afterInteractive">
+          {`
+    function initApollo() {
+      var n = Math.random().toString(36).substring(7);
+      var o = document.createElement("script");
+      o.src = "https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache=" + n;
+      o.async = true;
+      o.defer = true;
+      o.onload = function () {
+        if (window.trackingFunctions) {
+          window.trackingFunctions.onLoad({
+            appId: "699077d6a92ffe00154b2a68"
+          });
+        }
+      };
+      document.head.appendChild(o);
+    }
+    initApollo();
+  `}
+        </Script>
       </head>
       <body>
         {/* Google Tag Manager (noscript) */}
